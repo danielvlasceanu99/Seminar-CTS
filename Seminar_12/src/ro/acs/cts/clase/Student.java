5258ff917c9dc50a36ec48a59cf7888bf11fcc51
danielvlasceanu99 <danielvlasceanu99@gmail.com>
2021-05-22 13:10:28 +0300

    CTS - Seminar_12

--- Seminar_12/src/ro/acs/cts/clase/Student.java
+++ Seminar_12/src/ro/acs/cts/clase/Student.java
@@ -30,7 +30,11 @@ public class Student {
     }
 
     public void adaugaNota(int nota){
-        note.add(nota);
+        if(nota < 1 || nota > 10){
+            throw new IllegalArgumentException();
+        }else {
+            note.add(nota);
+        }
     }
 
     public int getNota(int index){
@@ -54,9 +58,9 @@ public class Student {
     public boolean areRestante() {
         for(int nota:note) {
             if(nota<5) {
-                return false;
+                return true;
             }
         }
-        return true;
+        return false;
     }
 }
--- Seminar_12/src/ro/acs/cts/test/StudentTest.java
+++ Seminar_12/src/ro/acs/cts/test/StudentTest.java
@@ -36,4 +36,75 @@ public class StudentTest {
         student.adaugaNota(nota);
         assertEquals(10, student.getNota(0));
     }
+
+    @Test
+    public void testGetNota(){
+        Student student = new Student();
+        student.adaugaNota(8);
+        student.adaugaNota(9);
+        student.adaugaNota(5);
+        student.adaugaNota(4);
+
+        assertEquals(5, student.getNota(2));
+    }
+
+    @Test
+    public void testAdaugaOSinguraNota(){
+        Student student = new Student();
+        int nota = 10;
+        student.adaugaNota(nota);
+        assertEquals(1, student.getNote().size());
+    }
+
+    @Test
+    public void testAdaugaNotaIncorecta(){
+        Student student = new Student();
+        //1
+        try{
+            //2
+            student.adaugaNota(-1);
+            //3 - nu ajunge aici
+            fail("nu trebuia sa ajunga aici. Metoda nu arunca exceptie");
+        }
+        catch (IllegalArgumentException ex){
+            //4
+        }
+        catch (Exception e){
+            //5 - nu ajunge aici
+            fail("Nu arunca o exceptie de tipul IllegalArgument");
+        }
+        //6
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAdaugaNotaIncorectaJunit4(){
+        Student student = new Student();
+        student.adaugaNota(-1);
+    }
+
+    @Test
+    public void testStudentAreRestante(){
+        Student student = new Student();
+        student.adaugaNota(3);
+        boolean rez = student.areRestante();
+        assertTrue(rez);
+    }
+
+    @Test
+    public void testStudentNuAreRestante(){
+        Student student = new Student();
+        student.adaugaNota(6);
+        boolean rez = student.areRestante();
+        assertFalse(rez);
+    }
+
+    @Test
+    public void testCalculeazaMedieCorect(){
+        Student student = new Student();
+        student.adaugaNota(10);
+        student.adaugaNota(9);
+
+        //assertEquals(6.5, student.calculeazaMedie());
+        assertEquals(9.5, student.calculeazaMedie(), 0.0001);
+    }
 }
